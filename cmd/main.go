e750a864febc1b01c0cf1595009c27125b7b8233
ThiagoBraz26 <thiagobrazpinheiro@gmail.com>
2026-03-04 09:42:47 -0300

    feat(main): add CLI entry point with host flag and elapsed timer

---
+++ cmd/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+	
+	"port-scanner/scanner"
+)
+
+func main() {
+	start := time.Now()
+	host := flag.String("host", "localhost", "Host alvo")
+	
+	ports := scanner.Run(*host)
+	
+	for _, port := range ports {
+		fmt.Println(port)
+	}
+
+	fmt.Printf("\n\n Tempo decorrido: %s", time.Since(start))
+}
