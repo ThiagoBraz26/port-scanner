dbd6e7ec65c1857d1f3bbc1bc4254f273df6beda
ThiagoBraz26 <thiagobrazpinheiro@gmail.com>
2026-03-04 09:42:16 -0300

    feat(scanner): add TCP port scanning for all ports

---
+++ scanner/scanner.go
@@ -0,0 +1,23 @@
+package scanner
+
+import (
+	"fmt"
+	"net"
+)
+
+func Run(host string) ([]string) {
+	var ports []string
+
+	for i := 0; i <= 65536; i++ {
+		conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", host, i))
+		if err != nil {
+			ports = append(ports, fmt.Sprintf("Porta: %-6d FECHADA", i))
+			continue
+		}
+
+		ports = append(ports, fmt.Sprintf("Porta: %-6d ABERTA", i))
+		conn.Close()
+	}
+	
+	return ports
+}
